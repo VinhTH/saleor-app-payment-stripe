fef9bda66b12a6e3ff36dfe1f236e32205a60284
Jason <vinhhuynh115@gmail.com>
2024-07-03 00:23:39 +0700

    Updated

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -1,25 +1,33 @@
 import { createAppRegisterHandler } from "@saleor/app-sdk/handlers/next";
+import { UpstashAPL } from "@saleor/app-sdk/APL";
+import { createLogger } from "@/lib/logger";
 
-import { saleorApp } from "../../saleor-app";
-import { env } from "@/lib/env.mjs";
-
-const allowedUrlsPattern = env.ALLOWED_DOMAIN_PATTERN;
-
-/**
- * Required endpoint, called by Saleor to install app.
- * It will exchange tokens with app, so saleorApp.apl will contain token
- */
 export default createAppRegisterHandler({
-  apl: saleorApp.apl,
-  allowedSaleorUrls: [
-    (url) => {
-      if (allowedUrlsPattern) {
-        const regex = new RegExp(allowedUrlsPattern);
-
-        return regex.test(url);
-      }
-
-      return true;
-    },
-  ],
+  apl: new UpstashAPL({
+    restURL: "https://wealthy-prawn-52787.upstash.io",
+    restToken: "Ac4zAAIncDFjZDIwNzYwMzk4ZDU0ZGJmOWQ1OTA2OTQxNzkwODEwMnAxNTI3ODc",
+  }),
+  allowedSaleorUrls: ["https://store-epbbnruq.saleor.cloud/graphql/"], // optional, see options below
+  async onRequestVerified(req, { authData, respondWithError }) {
+    const logger = createLogger({}, { msgPrefix: "onRequestVerified " });
+    logger.info("Token: " + authData.token + " ");
+    logger.info("Domain: " + authData.domain ?? "Empty" + " ");
+  },
+  async onAplSetFailed(req, { authData, respondWithError, error }) {
+    const logger = createLogger({}, { msgPrefix: "onAplSetFailed " });
+    logger.info("Token: " + authData.token + " ");
+    logger.info("Domain: " + authData.domain ?? "Empty" + " ");
+    logger.info(error);
+  },
+  async onAuthAplSaved(request, { authData, respondWithError }) {
+    const logger = createLogger({}, { msgPrefix: "onAuthAplSaved " });
+    logger.info("Token: " + authData.token + " ");
+    logger.info("Domain: " + authData.domain ?? "Empty" + " ");
+  },
+  async onRequestStart(request, { authToken, saleorApiUrl, saleorDomain, respondWithError }) {
+    const logger = createLogger({}, { msgPrefix: "onRequestStart " });
+    logger.info("Token: " + authToken + " ");
+    logger.info("Domain: " + saleorDomain ?? "Empty" + " ");
+    logger.info("Url: " + saleorApiUrl ?? "Empty" + " ");
+  },
 });
